c059657c6ae15042fe75fe5476c5fe6ad6622f44
Bui Hieu Thien <hieuthien95@gmail.com>
2019-03-10 15:15:35 +0700

    them variadic functuon interface

--- week1/function/main.go
+++ week1/function/main.go
@@ -4,14 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"reflect"
 )
 
-func demoVariadicFunction(item string, list ...int) {
-	list[0] = 999
-	fmt.Println(item, list)
-}
-
-// 3. function normal
+// 1. function normal
 func add(a, b int) (int, error) {
 	if a == 0 && b == 0 {
 		return 0, errors.New("mat vui roi")
@@ -22,12 +18,25 @@ func sayHi(s string) {
 	fmt.Println("Hi", s, "!")
 }
 
-// 4. function Values
+// 2a. Variadic Function: neu truyen vao array param thi la value type
+// 2b. 					  neu truyen vao slide..., thì nó là ref type
+// 2c. 					  different types of arguments
+func demoVariadicFunction(item string, list ...int) {
+	list[0] = 999
+	fmt.Println(item, list)
+}
+func variadicDiffType(i ...interface{}) {
+	for _, v := range i {
+		fmt.Println(v, "--", reflect.ValueOf(v).Kind())
+	}
+}
+
+// 3. function Values
 func funValues(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
-// 5. function Closures
+// 4. function Closures
 func adderFunClosures() func(int) int {
 	sum := 0
 	return func(x int) int {
@@ -36,21 +45,13 @@ func adderFunClosures() func(int) int {
 	}
 }
 
-// 1. neu truyen vao array param thi la value type
-// 2. neu truyen vao slide..., thì nó là ref type
-// 3. function normal
-// 4. function Values
-// 5. function Closures
+// 1. function normal
+// 2. Variadic Function: neu truyen vao array param thi la value type
+//   					 neu truyen vao slide..., thì nó là ref type
+// 3. function Values
+// 4. function Closures
 func main() {
-	// 1. neu truyen vao array param thi la value type
-	demoVariadicFunction("echo:", 1, 2, 3, 4, 5, 6, 7)
-	array := []int{1, 2, 3}
-
-	// 2. neu truyen vao slide..., thì nó là ref type
-	demoVariadicFunction("echo:", array...)
-	fmt.Println(array)
-
-	// 3. function normal
+	// 1. function normal
 	sayHi("Thiện")
 	//
 	sum, err := add(0, 0)
@@ -60,7 +61,16 @@ func main() {
 		fmt.Println(sum)
 	}
 
-	// 4.
+	// 2a. neu truyen vao array param thi la value type
+	demoVariadicFunction("echo:", 1, 2, 3, 4, 5, 6, 7)
+	array := []int{1, 2, 3}
+	// 2b. neu truyen vao slide..., thì nó là ref type
+	demoVariadicFunction("echo:", array...)
+	fmt.Println(array)
+	// 2c.
+	variadicDiffType(1, "red", true, 1.5, []string{"foo", "bar"}, map[string]int{"apple": 1, "tomato": 2})
+
+	// 3. function Values
 	tenFuncVal := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
@@ -69,7 +79,7 @@ func main() {
 	fmt.Println(funValues(tenFuncVal))
 	fmt.Println(funValues(math.Pow))
 
-	// 5.
+	// 4. function Closures
 	// funcName := adderFunClosures()
 
 	// num := funcName(1)

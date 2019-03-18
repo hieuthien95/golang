0f4533e91c008cce3aa291af695dcf869ca8a598
thienbh <hieuthien95@gmail.com>
2019-03-18 20:39:40 +0700

    mimiddleware.LoggerWithConfig them sample get tu other API, Marshal

--- echo/crud-mgo/main.go
+++ echo/crud-mgo/main.go
@@ -184,6 +184,9 @@ func main() {
 
 	// Middleware
 	// e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
 	e.Use(middleware.Recover())
 
 	// Routes

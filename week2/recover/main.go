e480520b76d5cd1e90ae3b2fee331e81a9233d64
thienbh <hieuthien95@gmail.com>
2019-03-24 21:38:26 +0700

    edit recover example

--- week2/recover/main.go
+++ week2/recover/main.go
@@ -2,22 +2,29 @@ package main
 
 import "fmt"
 
+func looop() {
+	var action int
+	for {
+		fmt.Println("Enter 1 for Student and 2 for Professional")
+		fmt.Scanln(&action)
+
+		switch action {
+		case 1:
+			fmt.Println("I am a  Student")
+		case 2:
+			fmt.Println("I am a  Professional")
+		default:
+			panic(fmt.Sprintf("I am a  %d", action))
+		}
+	}
+}
+
 func main() {
 	defer func() {
 		action := recover()
 		fmt.Println("recover: stopped", action)
+		main()
 	}()
 
-	var action int
-	fmt.Println("Enter 1 for Student and 2 for Professional")
-	fmt.Scanln(&action)
-
-	switch action {
-	case 1:
-		fmt.Println("I am a  Student")
-	case 2:
-		fmt.Println("I am a  Professional")
-	default:
-		panic(fmt.Sprintf("I am a  %d", action))
-	}
+	looop()
 }

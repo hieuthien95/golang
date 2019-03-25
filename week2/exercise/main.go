8543bea5b084baec413ed4682c2019e90d3420c3
thienbh <hieuthien95@gmail.com>
2019-03-25 10:15:14 +0700

    nop bt week2

--- week2/exercise/main.go
+++ week2/exercise/main.go
@@ -4,44 +4,125 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
-	"./crawler"
+	"crawl-module/crawler"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db             *gorm.DB
+	chanUrls       chan Url
+	chanActicles   chan Article
+	chanUrlsUpdate chan Url
+)
+
+type (
+	Url struct {
+		Id     int
+		Url    string
+		Status int
+	}
+
+	Article struct {
+		crawler.Data
+		Id int
+	}
 )
 
 var SaiGonTime crawler.ICrawler = crawler.CreateSaiGonTimeCrawler()
-var VietNamNet crawler.ICrawler = crawler.CreateVietNamNetCrawler()
+
+// var VietNamNet crawler.ICrawler = crawler.CreateVietNamNetCrawler()
 
 func main() {
-	url := "https://www.thesaigontimes.vn/274113/bao-giay-van-thu-vi.html"
-	resp, _ := http.Get(url)
-	data := SaiGonTime.Parse(resp)
-	b, _ := json.Marshal(data)
-	fmt.Println(string(b))
+	db, _ = gorm.Open("mysql", "root:123456789@/golang?charset=utf8&parseTime=True&loc=Local")
+	db.AutoMigrate(Url{}, Article{})
+
+	chanUrls = make(chan Url)
+	chanUrlsUpdate = make(chan Url)
+	chanActicles = make(chan Article)
+
+	go load()
+	go crawl()
+	go save()
+	go update()
 
 	// url := "https://vietnamnet.vn/vn/cong-nghe/ung-dung/cach-su-dung-google-maps-de-giam-sat-vi-tri-cua-tre-nho-514378.html"
 	// resp, _ := http.Get(url)
 	// data := VietNamNet.Parse(resp)
 	// b, _ := json.Marshal(data)
 	// fmt.Println(string(b))
-	// defer resp.Body.Close()
 
-	// go load()
-	// go crawl()
-	// go save()
-	// go update()
+	time.Sleep(time.Second * 20)
 }
 
 func load() {
+	urls := []Url{}
+
+	// if err := db.Find(&urls, "status <> 1").Error; err != nil {
+	if err := db.Find(&urls).Error; err != nil {
+		panic("db.Find(&urls).Error")
+	}
+
+	for _, url := range urls {
+		chanUrls <- url
+	}
 }
 
 func crawl() {
+	for {
+		url := <-chanUrls
+		chanUrlsUpdate <- url
 
+		resp, err := http.Get(url.Url)
+		if err == nil {
+			defer resp.Body.Close()
+		}
+
+		data := SaiGonTime.Parse(resp)
+		b, _ := json.Marshal(data.Title)
+		fmt.Println(string(b))
+
+		article := Article{}
+		article.Title = data.Title
+		article.PublishedDate = data.PublishedDate
+		article.Author = data.Author
+		article.Content = data.Content
+
+		chanActicles <- article
+	}
 }
 
 func save() {
+	articles := []Article{}
 
+	for {
+		articles = append(articles, <-chanActicles)
+
+		if len(articles) >= 5 {
+			for _, atc := range articles {
+				atc.Id = 0
+				err := db.Save(&atc).Error
+				if err != nil {
+					panic(err)
+				}
+			}
+			articles = []Article{}
+		}
+	}
 }
 
 func update() {
+	for {
+		url := <-chanUrlsUpdate
 
+		url.Status = 1
+		err := db.Save(&url).Error
+
+		if err != nil {
+			panic("db.Save(&url).Error")
+		}
+	}
 }

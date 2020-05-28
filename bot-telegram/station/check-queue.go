35cbeceb1eed861bd2459078ded27a902134c806
ThienBH <thienbh@ghn.vn>
2020-05-28 14:08:32 +0700

    sap xep, them channel

--- bot-telegram/station/check-queue.go
+++ bot-telegram/station/check-queue.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -17,7 +20,7 @@ var (
 )
 
 func sendMessage(chatID int, msg string) {
-	url := fmt.Sprintf("https://api.telegram.org/bot%v/sendMessage?chat_id=%v&text=%v", token, chatID, msg)
+	url := fmt.Sprintf("https://api.telegram.org/bot%v/sendMessage?chat_id=%v&text=%v", token, chatID, url.QueryEscape(msg))
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -80,12 +83,14 @@ func getUpdateMessage() {
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	var update UpdateMessage
@@ -93,21 +98,28 @@ func getUpdateMessage() {
 	err = json.Unmarshal(body, &update)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	for _, elm := range update.Result {
 		offset = elm.UpdateID + 1
 
 		// getUpdateActionStation
-		if elm.Message.Text == "/check" {
+		if elm.Message.Text == "/station" {
 			getUpdateActionStation()
 			sendMessage(-410940764, "Done")
 			return
 		}
 		// getUpdateActionPayroll
-		if elm.Message.Text == "/check-payroll" {
+		if elm.Message.Text == "/payroll" {
 			getUpdateActionPayroll()
 			sendMessage(-410940764, "Done")
+			return
+		}
+		// channel
+		if strings.Contains(elm.Message.Text, "/chan") {
+			getUpdateActionChannel(elm.Message.Text)
+			sendMessage(-427411096, "Done")
 			return
 		}
 
@@ -120,7 +132,25 @@ func getUpdateMessage() {
 	// fmt.Println(string(body))
 }
 
-type queue struct {
+// ========================================================================================================================
+
+// Env ...
+type Env struct {
+	env   string
+	token string
+	items []Item
+}
+
+// Item ...
+type Item struct {
+	system     string
+	baseURL    string
+	queueTypes []string
+}
+
+// ========================================================================================================================
+
+type QueueResp struct {
 	Status string `json:"status,omitempty"`
 	Data   []struct {
 		ID              string    `json:"_id,omitempty"`
@@ -137,23 +167,36 @@ type queue struct {
 	Total   int    `json:"total,omitempty"`
 }
 
-// Station ...
-type Station struct {
-	env   string
-	token string
-	items []Item
-}
+func callRestStation(url string, authen string) (resp QueueResp, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return resp, err
+	}
 
-// Item ...
-type Item struct {
-	system     string
-	baseURL    string
-	queueTypes []string
+	req.Header.Add("Authorization", authen)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return resp, err
+	}
+
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
 }
 
 func getUpdateActionStation() {
 
-	arr := []Station{}
+	arr := []Env{}
 
 	for _, check := range arr {
 		for _, item := range check.items {
@@ -162,7 +205,7 @@ func getUpdateActionStation() {
 				url := item.baseURL + "?queueType=" + qType + "&limit=1"
 				infoMessage := check.env + " - " + item.system + " - " + qType + " - " + fmt.Sprint(time.Now().In(tz))
 
-				queue, err := callRest(url, check.token)
+				resp, err := callRestStation(url, check.token)
 				if err != nil {
 					// send message
 					sendMessage(-410940764, infoMessage)
@@ -174,17 +217,15 @@ func getUpdateActionStation() {
 
 					fmt.Println()
 
-				} else if len(queue.Data) > 0 && len(queue.Data[0].Log) >= 5 {
-					content := queue.Data[0].Log[0]
+				} else if len(resp.Data) > 0 && len(resp.Data[0].Log) >= 5 {
 
 					// send message
 					sendMessage(-410940764, infoMessage)
-					sendMessage(-410940764, content)
+					sendMessage(-410940764, resp.Data[0].Log[0])
 
 					// console
 					fmt.Println(infoMessage)
-
-					byte, _ := json.Marshal(queue)
+					byte, _ := json.Marshal(resp)
 					fmt.Println(string(byte))
 
 					fmt.Println()
@@ -195,16 +236,52 @@ func getUpdateActionStation() {
 
 }
 
+// ========================================================================================================================
+
+type PayrollResp struct {
+	Status  string   `json:"status,omitempty"`
+	Data    []string `json:"data,omitempty"`
+	Message string   `json:"message,omitempty"`
+}
+
+func callRestPayroll(url string, authen string) (data PayrollResp, err error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return data, err
+	}
+
+	req.Header.Add("Authorization", authen)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return data, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return data, err
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
 func getUpdateActionPayroll() {
 
-	arr := []Station{}
+	arr := []Env{}
 
 	for _, check := range arr {
 		for _, item := range check.items {
 
 			url := item.baseURL
 
-			resp, err := callRest(url, check.token)
+			resp, err := callRestPayroll(url, check.token)
 			if err != nil {
 				// send message
 				sendMessage(-410940764, err.Error())
@@ -215,14 +292,16 @@ func getUpdateActionPayroll() {
 				fmt.Println()
 
 			} else {
-				byte, _ := json.Marshal(resp.Data)
-				fmt.Println(string(byte))
+				datas := check.env + "\n"
+				for _, s := range resp.Data {
+					datas += s + "\n"
+				}
 
 				// send message
-				sendMessage(-410940764, string(byte))
+				sendMessage(-410940764, datas)
 
 				// console
-				fmt.Println(string(byte))
+				fmt.Println(resp.Data)
 
 				fmt.Println()
 			}
@@ -231,34 +310,126 @@ func getUpdateActionPayroll() {
 
 }
 
-func callRest(url string, authen string) (queue, error) {
-	var queue queue
+// ========================================================================================================================
 
-	req, err := http.NewRequest("GET", url, nil)
+type LichPhatSongResp struct {
+	LichPhatSong struct {
+		ChannelID int    `json:"ChannelId"`
+		Date      string `json:"Date"`
+		EventList []struct {
+			StartTime          string `json:"StartTime"`
+			EndTime            string `json:"EndTime"`
+			Name               string `json:"Name"`
+			ShortDescriptor    string `json:"ShortDescriptor"`
+			ExtendedDescriptor string `json:"ExtendedDescriptor"`
+			Hot                bool   `json:"Hot"`
+		} `json:"EventList"`
+	} `json:"LichPhatSong"`
+	End bool   `json:"End"`
+	Ext string `json:"Ext"`
+}
+
+type Kenh struct {
+	Ma  int    `json:"ma"`
+	Ten string `json:"ten"`
+}
+
+func callRestChannel(url string, bodyData map[string]interface{}) (data LichPhatSongResp, err error) {
+
+	byte, err := json.Marshal(bodyData)
 	if err != nil {
-		return queue, err
+		return data, err
 	}
 
-	req.Header.Add("Authorization", authen)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(byte))
+	if err != nil {
+		return data, err
+	}
+
+	// req.Header.Add("Authorization", authen)
+	req.Header.Add("Content-Type", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return queue, err
+		return data, err
 	}
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return queue, err
+		return data, err
 	}
 
-	err = json.Unmarshal(body, &queue)
+	err = json.Unmarshal(body, &data)
 	if err != nil {
-		return queue, err
+		return data, err
 	}
 
-	return queue, nil
+	return data, nil
 }
+
+func getUpdateActionChannel(kenh string) {
+	now := time.Now()
+	hnow := now.Format("15:04:05")
+
+	kenhs := []Kenh{
+		{Ma: 94, Ten: "Animal Planet"},
+		{Ma: 50, Ten: "AXN"},
+		{Ma: 90, Ten: "Cartoon Network"},
+		{Ma: 250, Ten: "Cinemax"},
+		{Ma: 92, Ten: "Discovery Channel"},
+		{Ma: 26, Ten: "FOX MOVIES"},
+		{Ma: 23, Ten: "HBO"},
+	}
+
+	kenh = strings.Replace(kenh, "/chan", "", 1)
+	kenh = strings.ToLower(kenh)
+
+	for _, k := range kenhs {
+		k.Ten = strings.ToLower(k.Ten)
+		if strings.Contains(k.Ten, kenh) {
+
+			body := make(map[string]interface{})
+			body["maKenh"] = k.Ma
+			body["ngay"] = now
+
+			URL := "https://www.sctv.com.vn/WebMain/LichPhatSong/LayLichPhatSong"
+
+			resp, err := callRestChannel(URL, body)
+			if err != nil {
+				// send message
+				sendMessage(-427411096, err.Error())
+
+				// console
+				fmt.Println(err.Error())
+
+				fmt.Println()
+
+			} else {
+				datas := strings.ToUpper(k.Ten) + "\n ============================= \n\n"
+				for _, l := range resp.LichPhatSong.EventList {
+					if hnow <= l.StartTime || hnow <= l.EndTime {
+						datas += l.StartTime + " - " + l.Name + "\n\n"
+					}
+				}
+
+				// byte, _ := json.Marshal(resp.LichPhatSong.EventList)
+				// fmt.Println(string(byte))
+
+				// send message
+				sendMessage(-427411096, fmt.Sprint(datas))
+
+				// console
+				fmt.Println(datas)
+
+				fmt.Println()
+			}
+		}
+	}
+
+}
+
+// ========================================================================================================================
 
 func main() {
 	fmt.Println("Running ...")

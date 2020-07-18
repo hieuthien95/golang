246e9e9bc892cb7510ead4f3edaea7c0e4ac5a6c
ThienBH <thienbh@ghn.vn>
2020-07-18 13:53:26 +0700

    init main

--- LTDT/week3/exercise2 - bellman ford/main.go
+++ LTDT/week3/exercise2 - bellman ford/main.go
@@ -26,41 +26,28 @@ func main() {
 	g.MapEdge["6-2"] = 2
 	g.MapEdge["6-5"] = 1
 
-	g.MatrixA = [][]int{
-		{0, 3, 0, 0, 0, 0, 1},
-		{3, 0, 4, 0, 0, 0, 0},
-		{0, 4, 0, 0, 0, 0, 2},
-		{0, 0, 0, 0, 5, 0, 0},
-		{0, 0, 0, 5, 0, 1, 0},
-		{0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 2, 0, 0, 1, 0},
-	}
-
 	g.bellmanFord(0)
-
-	fmt.Println(g.L)
-	fmt.Println(g.P)
 }
 
 // Graph ...
 type Graph struct {
-	n       int
-	MatrixA [][]int
+	n int
+	// MatrixA [][]int
 	MapEdge map[string]int
 
-	P []int
-	L []int
+	P      []int
+	Weight []int
 }
 
 func (g *Graph) bellmanFord(start int) {
 	//Bước khởi tạo giống mã giả
 	g.P = make([]int, g.n)
-	g.L = make([]int, g.n)
+	g.Weight = make([]int, g.n)
 
-	for i := 0; i < len(g.L); i++ {
-		g.L[i] = 999999999999
+	for i := 0; i < len(g.Weight); i++ {
+		g.Weight[i] = 999999999999
 	}
-	g.L[start] = 0
+	g.Weight[start] = 0
 
 	//Biến xác định lặp n lần không biết trước
 	stop := false
@@ -75,10 +62,10 @@ func (g *Graph) bellmanFord(start int) {
 			j, _ := strconv.Atoi(strings.Split(key, "-")[1])
 
 			if g.MapEdge[key] > 0 && g.MapEdge[key] < 999999999999 {
-				if g.L[j] > g.L[i]+g.MapEdge[key] {
+				if g.Weight[j] > g.Weight[i]+g.MapEdge[key] {
 
-					g.L[j] = g.L[i] + g.MapEdge[key]
-					// g.P[j] = i
+					g.Weight[j] = g.Weight[i] + g.MapEdge[key]
+					g.P[j] = i
 
 					stop = false
 				}
@@ -93,51 +80,23 @@ func (g *Graph) bellmanFord(start int) {
 			stop = true
 		}
 	}
+
+	g.viewPathBellmanFord(start)
 }
 
-// func (g *Graph) bellmanFord(source int) {
+func (g *Graph) viewPathBellmanFord(start int) {
 
-// 	// Vì mãng của Phong nhận vào load từ file nên chô nào vô cùng là Phong để số 0.Nên khi vô code ta phải chuyển nó về giá trị max value
-// 	for i := 0; i < len(g.MatrixA); i++ {
-// 		for j := 0; j < len(g.MatrixA); j++ {
-// 			if g.MatrixA[i][j] == 0 {
-// 				g.MatrixA[i][j] = 999999999999
-// 			}
-// 		}
-// 	}
-// 	//Bước khởi tạo giống mã giả
-// 	g.P = make([]int, g.n)
-// 	g.L = make([]int, g.n)
-// 	for i := 0; i < len(g.L); i++ {
-// 		g.L[i] = 999999999999
-// 	}
-// 	g.L[source] = 0
-// 	//Biến xác định lặp n lần không biết trước
-// 	stop := false
-// 	k := 0
-// 	for !stop {
-// 		stop = true
-// 		k = k + 1
-// 		//Đây là phần quan trọng của thuật toán tập trung vào câu if
-// 		for i := 0; i < len(g.L); i++ {
-// 			for j := 0; j < len(g.L); j++ {
-// 				if g.MatrixA[i][j] > 0 && g.MatrixA[i][j] < 999999999999 {
-// 					//Gần như đây là công thức nên cũng không khó hiểu phải hông nè.Phần này buộc các bạn phải chạy tay hoặc debug mới hiểu nguyên tắc.Mình hông biết phải nói như thế nào khi viết text nữa,hehe
-// 					if g.L[j] > g.L[i]+g.MatrixA[i][j] {
-// 						// System.out.println(i + " " + j + " " + g.MatrixA[i][j]);
-// 						g.L[j] = g.L[i] + g.MatrixA[i][j]
-// 						g.P[j] = i
-// 						stop = false
-// 					}
-// 				}
-// 			}
-// 		}
-// 		//Đây là phần kiểm tra chu trình âm
-// 		if k > g.n {
-// 			if stop == false {
-// 				fmt.Println("Graph have negative cycle")
-// 			}
-// 			stop = true
-// 		}
-// 	}
-// }
+	for i := 0; i < g.n; i++ {
+		p := ""
+
+		//DÙng mãng P và đỉnh kết thúc để truy suất từng phần tử đã lưu trong P
+		vertex := i
+		for vertex != start {
+			vertex = g.P[vertex]
+			p = fmt.Sprint(vertex, "-", p)
+		}
+
+		p += fmt.Sprint(i) + fmt.Sprint(" = ", g.Weight[i])
+		fmt.Println(p)
+	}
+}
